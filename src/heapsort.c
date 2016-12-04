1803f99d4421753dd77be386bd22ac245d9fd8fe
Martin Knoblauch Revuelta <mkrevuelta@gmail.com>
2016-12-04 21:43:39 +0100

    Move LICENSE to src directory.
    Source will be under MIT license.
    The documentation, instead, will be under CC BY 3.0.

--- src/heapsort.c
+++ src/heapsort.c
@@ -1,6 +1,6 @@
 /*
     Copyright (c) 2013, Martin Knoblauch Revuelta
-    See accompanying ../LICENSE
+    See accompanying LICENSE
     
     ---------------------------------------------------------------
     heapsort.c
